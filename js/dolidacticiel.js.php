e26a02b59a8b591ea4159085bd52a676ad307a80
atm-ph <phf@atm-consulting.fr>
2015-07-18 17:50:20 +0200

    FIX : placement des bulles d'aides si des élèments html sont déplacés par un autre JS

--- js/dolidacticiel.js.php
+++ js/dolidacticiel.js.php
@@ -8,30 +8,29 @@
 ?>
 
 $(document).ready(function() {
-    
-
-    <?php
-      
-        foreach($Tab as &$d) {
-            
-            if(!empty($d->tips) && !$d->currentUserAchievement) {
-               
-                echo 'var obj = $(\''.$d->tips.'\').first();
-                var offset = obj.offset();
-                if(offset && offset.top>0) {
-                    
-                    $("body").append(\'<div style="position:absolute; left:\'+(offset.left-20)+\'px; top:\'+(offset.top - 10)+\'px;" class="dolidacticielTips"><span class="code">'.$d->code.'</span><div class="content"><strong>'.$d->title.'</strong><br />'.$d->description.'</div></div>\');
-                    
-                }
-               
-                ';
-                
-            }
-            
-            
-        }
-        
-    
-    ?>
+	
+	$(window).bind("load", function() {
+	    <?php
+	    
+	        foreach($Tab as &$d) {
+	            
+	            if(!empty($d->tips) && !$d->currentUserAchievement) {
+	               
+	                echo 'var obj = $(\''.$d->tips.'\').first();
+	                var offset = obj.offset();
+	                if(offset && offset.top>0) {
+	                    
+	                    $("body").append(\'<div style="position:absolute; left:\'+(offset.left-20)+\'px; top:\'+(offset.top - 10)+\'px;" class="dolidacticielTips"><span class="code">'.$d->code.'</span><div class="content"><strong>'.$d->title.'</strong><br />'.$d->description.'</div></div>\');
+	                    
+	                }
+	               
+	                ';
+	                
+	            } 
+	        }
+	    
+	    ?>
+	    
+	});
 
 });
